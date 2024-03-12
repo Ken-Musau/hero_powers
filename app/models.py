e7992ca38f50857d1cfeb30d60947612cbf89656
Kennedy Musau <kennedy.musauu@gmail.com>
2024-03-12 06:22:58 +0300

    add serialization

--- app/models.py
+++ app/models.py
@@ -1,14 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Nullable
+from sqlalchemy_serializer import SerializerMixin
+
 
 db = SQLAlchemy()
 
 
-class Hero(db.Model):
+class Hero(db.Model, SerializerMixin):
     __tablename__ = 'heroes'
 
+    serialize_rules = ("-hero_powers.hero",)
+
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, Nullable=False)
+    name = db.Column(db.String, nullable=False)
     super_name = db.Column(db.String)
     created_at = db.Column(db.DateTime(), server_default=db.func.now())
     updated_at = db.Column(db.DateTime(), onupdate=db.func.now())
@@ -16,8 +19,10 @@ class Hero(db.Model):
     hero_powers = db.relationship("HeroPower", backref="heroes")
 
 
-class HeroPower(db.Model):
+class HeroPower(db.Model, SerializerMixin):
     __tablename__ = "hero_powers"
+
+    serialize_rules = ("-power.hero_powers", "-hero.hero_powers",)
 
     id = db.Column(db.Integer, primary_key=True)
     strength = db.Column(db.String)
@@ -25,13 +30,20 @@ class HeroPower(db.Model):
     updated_at = db.Column(db.DateTime(), onupdate=db.func.now())
 
     hero_id = db.Column(db.Integer, db.ForeignKey("heroes.id"))
+    power_id = db.Column(db.Integer, db.ForeignKey("powers.id"))
 
 
-class Power(db.Model):
+class Power(db.Model, SerializerMixin):
     __tablename__ = "powers"
 
+    serialize_rules = ("-hero_powers.power.",)
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    description = db.Column(db.String)
+    created_at = db.Column(db.DateTime(), server_default=db.func.now())
+    updated_at = db.Column(db.DateTime(), onupdate=db.func.now())
+
+    hero_powers = db.relationship("HeroPower", backref="powers")
 
 
 # add any models you may need.
